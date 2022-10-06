2bd1c086978bdbb0d97efdd08cd790705310cbff
Daniel <daniel.rueda@ibisdev.tech>
2022-10-06 12:45:21 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,55 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import {useState} from 'react'
+
+export default function App() {
+  const [enteredGoalText,setEnteredGoalText] = useState('')
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  };
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler}/>              
+        <Button title='Add Goal' onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>
+          Your goals...
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer:{
+    flex:1,    
+    paddingTop:50,
+    paddingHorizontal:16,
+  },
+  inputContainer: {
+    flex:1,
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth:1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor: '#cccccc',
+    width:'70%',
+    marginRight:8,
+    padding:8
+  },
+  goalsContainer:{
+    flex:5
+  }
+});
